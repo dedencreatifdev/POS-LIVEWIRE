3ed5ada8721c46873dcd07f968b3dbaad7adf388
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-01-23 10:59:17 +0700

    .

---
+++ app/Livewire/Admin/Produk/ProdukList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Produk;
+
+use Livewire\Component;
+
+class ProdukList extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.produk.produk-list');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,28 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/',DashboardIndex::class)->name('Dashboard.Index');
-Route::get('/produk-list',DashboardIndex::class)->name('Produk.Index');
-Route::get('/produk',DashboardIndex::class)->name('Produk.List');
-Route::get('/pesanan',DashboardIndex::class)->name('Pesanan.Index');
-Route::get('/pesanan-list',DashboardIndex::class)->name('Pesanan.List');
-Route::get('/penjualan',DashboardIndex::class)->name('Penjualan.List');
-Route::get('/pembelian',DashboardIndex::class)->name('Pembelian.List');
-Route::get('/pembayaran',DashboardIndex::class)->name('Pembayaran.List');
-Route::get('/mutasi-stok',DashboardIndex::class)->name('MutasiStok.List');
-Route::get('/opname-stok',DashboardIndex::class)->name('Opname.List');
+// DASHBOARD
+Route::get('/', DashboardIndex::class)->name('Dashboard.Index');
+
+// PRODUK
+Route::get('/produk-list', DashboardIndex::class)->name('Produk.Index');
+Route::get('/produk', DashboardIndex::class)->name('Produk.List');
+
+// PESANAAN
+Route::get('/pesanan', DashboardIndex::class)->name('Pesanan.Index');
+Route::get('/pesanan-list', DashboardIndex::class)->name('Pesanan.List');
+
+// PENJUALAN
+Route::get('/penjualan', DashboardIndex::class)->name('Penjualan.List');
+
+// PEMBELIAN
+Route::get('/pembelian', DashboardIndex::class)->name('Pembelian.List');
+
+// PEMBAYARAN
+Route::get('/pembayaran', DashboardIndex::class)->name('Pembayaran.List');
+
+// MUTASI STOK
+Route::get('/mutasi-stok', DashboardIndex::class)->name('MutasiStok.List');
+
+// OPNAME STOK
+Route::get('/opname-stok', DashboardIndex::class)->name('Opname.List');
